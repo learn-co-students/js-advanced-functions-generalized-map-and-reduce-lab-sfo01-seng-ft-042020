824b9d6986a3fc005c9f83cc353acc239f7e8e69
tharveybrown <tharveybrown@gmail.com>
2020-06-11 14:25:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(src, fn) {
+  let r = [];
+  for(let i = 0; i < src.length; i++) {
+    r.push(fn(src[i]))
+  }
+  return r;
+}
+
+function reduce(src, fn, start) {
+  let r = (!!start) ? start : src[0];
+  let i = (!!start) ? 0 : 1
+
+  for(i; i < src.length; i++) {
+    r = fn(src[i], r)
+
+  }
+  return r
+}
