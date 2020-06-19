a01b9c41017337831c0887b3937f3ed3d0ef848b
Seanbon0611 <seand0611@gmail.com>
2020-06-18 17:01:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+const map = (arr, callback) => arr.map(el => callback(el));
+
+const reduce = (arr, callback, start) => {
+  let accumulator = start ? start : arr[0];
+  let i = start ? 0 : 1;
+
+  for (; i < arr.length; i++) {
+    accumulator = callback(arr[i], accumulator);
+  }
+
+  return accumulator;
+}
+// arr.reduce(callback( accumulator, currentValue[, index[, array]] )[, initialValue])
